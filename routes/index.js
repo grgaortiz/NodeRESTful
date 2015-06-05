7ab3c74e4db38db9340a254e8fde37f6396e873e
grgaortiz <grgaortiz@gmail.com>
2015-06-05 12:07:58 -0600

    email verify test

--- routes/index.js
+++ routes/index.js
@@ -168,7 +168,7 @@ exports.getOne = function (req, res) {
 
 exports.getUrl = function (req, res) {
     var email = req.param('email');
-    verifier.verify('tedra.white@promedica.org', function (err, info) {
+    verifier.verify('tedra@promedica.org', function (err, info) {
         if (err) console.log(err);
         else {
             var jsonData = {
